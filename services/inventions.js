d8dd5271d1356578ba45993a02cb12025ec27e78
Leaula21 <lola.mounier.lm@gmail.com>
2024-02-05 15:50:58 +0100

    commit

--- services/inventions.js
+++ services/inventions.js
@@ -64,6 +64,14 @@ function displayInvention() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
     // Get the target element from the DOM
+    const dataIdd = document.getElementById ("invention_title")
+ const dataId = document.getElementById("invention_details")
     // Get the invention object from the array "inventions" using the id
+let item = inventions.find(item => item.id == id)
+console.log (item)
     // Display the details of the invention in the target element
+dataIdd.innerHTML = item.name
+dataId.innerHTML += `<img src="/assets/images/inventions/${item.image}" alt="${item.name}">
+<p>Inventé par ${item.inventor} en ${item.year}</p>
+<p>${item.description}</p>`
 }
